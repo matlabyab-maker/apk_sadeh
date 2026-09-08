55c32e874d4b54d5a77e9204cceec54bf68c1f51
matlabyab-maker <matlabyab@gmail.com>
2026-09-08 10:15:56 +0330

    Add files via upload

---
+++ .github/workflows/MainActivity.java
@@ -0,0 +1,22 @@
+package com.fastradio.lite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("Fast Radio");
+        text.setTextSize(28);
+        text.setTextColor(Color.BLACK);
+        text.setGravity(Gravity.CENTER);
+
+        setContentView(text);
+    }
+}
